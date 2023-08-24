bb8d2a258603feee15ac4e8e0ebcfb5273c13f4d
Greg Doud <gpdoud@gmail.com>
2023-08-24 08:55:14 -0400

    Add project files.

---
+++ ExceptionExamples-C41/BootcampException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionExamples_C41;
+
+internal class BootcampException : Exception {
+
+    public int Numerator { get; set; }
+    public int Denominator { get; set; }
+
+    public BootcampException() : base() { }
+
+    public BootcampException(string message) : base(message) { }
+
+    public BootcampException(string message,  Exception innerException) 
+        : base(message, innerException) { }
+}
---
+++ ExceptionExamples-C41/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using ExceptionExamples_C41;
+
+try {
+    method1();
+} catch (Exception ex) {
+
+}
+
+void method1() {
+    method2();
+}
+
+void method2() {
+    method3();
+}
+
+void method3() {
+    method4();
+}
+
+void method4() {
+    var d = 0;
+    var e = 1 / d;
+}
